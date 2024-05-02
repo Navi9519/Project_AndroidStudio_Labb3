dd2dce8b5796733356edf4c6473eadb45d20a5f9
Navi9519 <dedicivanovic@gmail.com>
2024-05-02 13:11:52 +0200

    switched to navcontroller from destinations and implemented mutable state logic in signupscreen -> now test for saving user in DB

--- app/src/main/java/com/navi9519/labb_3_cocktail_app/view/composables/screens/SignUpScreen.kt
+++ app/src/main/java/com/navi9519/labb_3_cocktail_app/view/composables/screens/SignUpScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -30,11 +32,13 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.navi9519.labb_3_cocktail_app.R
+import com.navi9519.labb_3_cocktail_app.model.database.user.User
 import com.navi9519.labb_3_cocktail_app.model.database.user.UserRepository
 import com.navi9519.labb_3_cocktail_app.view.composables.AccountOrNot
 import com.navi9519.labb_3_cocktail_app.view.composables.Btn
 import com.navi9519.labb_3_cocktail_app.view.composables.SignInInputField
 import com.navi9519.labb_3_cocktail_app.view.composables.Title
+import kotlinx.coroutines.Dispatchers
 
 
 @Composable
@@ -44,7 +48,11 @@ fun SignUpScreen(
 
 ) {
 
-    var hej = "Hej"
+    // Mutable textField states for username and password
+    val usernameState = remember { mutableStateOf(TextFieldValue("")) }
+    val emailState = remember { mutableStateOf(TextFieldValue("")) }
+    val passwordState= remember { mutableStateOf(TextFieldValue("")) }
+
 
     Box(modifier = Modifier
         .fillMaxSize()
@@ -75,7 +83,7 @@ fun SignUpScreen(
 
                 null,
 
-                 Stroke(
+                Stroke(
                     miter = 10f,
                     width = 16f,
                     join = StrokeJoin.Miter
@@ -90,31 +98,32 @@ fun SignUpScreen(
 
             // Username input field logic
             SignInInputField(
-                TextFieldValue(),
-                { newValue -> hej = newValue.toString() },
+                usernameState.value,
+                { newValue -> usernameState.value = newValue },
                 icon = "person",
                 placeholder = "Username",
             )
 
+            // Email input field logic
             SignInInputField(
-                TextFieldValue(),
-                { newValue -> hej = newValue.toString() },
+                emailState.value,
+                { newValue -> emailState.value = newValue },
                 icon = "email",
-               placeholder = "E-mail",
+                placeholder = "E-mail",
             )
 
             // Password input field logic
             SignInInputField(
-                TextFieldValue(),
-                { newValue -> hej = newValue.toString() },
+                passwordState.value,
+                { newValue -> passwordState.value = newValue },
                 icon = "lock",
-               placeholder = "Password",
+                placeholder = "Password",
                 visual = PasswordVisualTransformation()
             )
 
             SignInInputField(
-                TextFieldValue(),
-                { newValue -> hej = newValue.toString() },
+                passwordState.value,
+                { newValue -> passwordState.value = newValue },
                 icon = "lock",
                 placeholder = "Confirm Password",
                 visual = PasswordVisualTransformation()
@@ -122,9 +131,25 @@ fun SignUpScreen(
 
 
             Btn("Sign Up") {
-                navController.navigate("LoginScreen")
-            }
 
+                // Setting more easy readable variables for state changes
+                val username = usernameState.value.text
+                val email = emailState.value.text
+                val password = passwordState.value.text
+
+
+                if (username.length >= 2 && email.length >= 10 && password.length >= 6) {
+
+                    // Check if the username already exists i users(list)
+
+                    // Add new User object
+                    val newUser = User(email, username, password)
+
+                    // Update the userDatabase state, with the new user
+                    
+
+                }
+            }
 
             Row() {
                 AccountOrNot(
